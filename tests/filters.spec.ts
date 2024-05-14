ad773c07f6deb34431650a75b8de63aea6127774
rafal.sipiatowski <Rafal.Sipiatowski@home.eu>
2024-05-14 17:26:18 +0200

    wizualne porównanie

---
+++ tests/filters.spec.ts
@@ -0,0 +1,6 @@
+import { test } from '@playwright/test';
+
+test('Filtry', async ({ page }) => {
+    await page.goto('https://spree-multi-vendor-demo.herokuapp.com/');
+    await page.locator('.main-nav-bar-item').filter({ hasText: 'Contact us' }).getByRole('link').click();
+});
